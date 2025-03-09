2c7c9303502e1422b693c2e1d90e70918b0f398a
Mijan2001 <mijan.cse19@gmail.com>
2025-03-09 06:29:12 +0600

    url added

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000/api/v1/users';
-// const API_URL =
-//     'https://advanced-authentication-mern-k759.onrender.com/api/v1/users';
+// const API_URL = 'http://localhost:8000/api/v1/users';
+const API_URL = 'https://advanced-auth-mern-nextjs.onrender.com/api/v1/users';
 
 export const signup = async userData => {
     const response = await axios.post(`${API_URL}/signup`, userData);
