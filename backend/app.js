7df41356cd08a786fc400dc76a0c8f35475fa578
Mijan2001 <mijan.cse19@gmail.com>
2025-03-09 23:42:59 +0600

    updated

--- backend/app.js
+++ backend/app.js
@@ -23,7 +23,12 @@ app.use(cookieParser());
 //     })
 // );
 // any origin can access the api===========
-app.use(cors({ origin: '*' }));
+app.use(
+    cors({
+        origin: 'https://next-auth-typescript.netlify.app',
+        credentials: true
+    })
+);
 app.use(express.json({ limit: '50mb' }));
 
 // Users api urls============
